94323b863818490b0a1d2e037afaa74612820b01
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-17 00:29:01 +0530

    side

---
+++ src/components/answers/1-basic-project.jsx
@@ -0,0 +1,11 @@
+export function SidebarClass1(){
+
+  return <div className="flex">
+    <div className="bg-red-200 w-64 h-screen">
+      Sidebar
+    </div>
+    <div className="bg-green-100 flex-1">
+      Content
+    </div>
+  </div>
+}
