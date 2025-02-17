e5fe89a896ea3bc3e4c8b0460d48a940525a2893
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-17 19:31:14 +0530

    added bar transition

--- src/components/bars/SideBar.jsx
+++ src/components/bars/SideBar.jsx
@@ -1,6 +1,6 @@
 export function SideBar(){
 
-  return <div className=" h-screen w-96 bg-red-500 ">
+  return <div className=" tranistion-all ease-in-out delay-100  -translate-x-90 md:translate-x-0  h-screen w-96 bg-red-500 ">
 
   </div>
 }
