e803763c05a065d9f3a0a86295f55d467d4e1bc0
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-17 15:16:02 +0530

    added dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import TopBox from './components/TopBox'
+import { SidebarClass1 } from './components/answers/1-basic-project'
 function App() {
 
   return <>
-   <TopBox/>
+  <div className='h-screen bg-white dark:bg-amber-950 text-black dark:text-white'>
+    <button onClick={()=>{
+      document.querySelector("html").classList.toggle("dark")
+    }}>Toggle theme</button>
+
+    <div className='text-2xl dark:text-white  text-blue-500'>
+      Hi there
+    </div>
+
+  </div>
+  
   </>
- 
         
 }
 
--- src/components/answers/1-basic-project.jsx
+++ src/components/answers/1-basic-project.jsx
@@ -1,10 +1,10 @@
 export function SidebarClass1(){
 
   return <div className="flex">
-    <div className="bg-red-200 w-64 h-screen">
+    <div className=" transition-all ease-in-out delay-100 duration-150 md:w-96 md:translate-x-0 -translate-x-96 bg-red-200 w-8 h-screen">
       Sidebar
     </div>
-    <div className="bg-green-100 flex-1">
+    <div className="bg-green-100  h-screen w-full ">
       Content
     </div>
   </div>
